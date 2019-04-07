d51b1d459b735036ffaf5f36bd2fc3eb74d8242e
allegra312 <49374635+allegra312@users.noreply.github.com>
2019-04-07 20:06:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<html>
+    <head>
+        <title>Library</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="Style.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      
+            <div class="container-fluid">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Main Menu
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Homepage</a></li>
+                    </ul>
+                </div>   
+                <div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a class="active" href="#">Sign Up</a></li>
+                        <li><a href="#">Log In</a></li>    
+                    </ul>     
+                </div>
+
+            </div>
+   
+    </nav>
+
+    <div id="home">
+        <div class="landing-text">
+            <h1>Windsor Library</h1> 
+        </div>
+    </div>
+
+        <div class="padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="searchbook.jpeg">
+                    
+                      
+                    </div> 
+                    <div class="col-sm-6 text-center">
+                        <input type="text" placeholder="Search book title here"> 
+                    </div>
+                </div>  
+            </div>
+            </div> 
+        <div id="fixed"></div>  
+        
+        <footer class="container-fluid text-center">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h3>Contact Us</h3> 
+                    <br>
+                    <h4>Address and contact info here.</h4>
+                </div>
+                <div class="col-sm-4">
+                    <h3>Connect</h3>  
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-google"></a>
+                </div>
+                <div class="col-sm-4">
+                    <img src="Windsor.png" class="icon">  
+                </div>
+                
+                
+                
+                
+            </div>
+        </footer>
+</body>
+</html>
